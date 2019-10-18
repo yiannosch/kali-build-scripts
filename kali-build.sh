d21157e39a7d85c6d32e46d3e77b33079c141f22
yiannosch <yiannos.ch90@gmail.com>
2019-10-18 12:59:27 +0100

    Finally merged firefox add-on script. Other fixes: docker, SoapUI, Nessus, drupwn etc.

--- kali-build.sh
+++ kali-build.sh
@@ -92,10 +92,10 @@ fi
 #### Update OS ####
 (( STAGE++ ))
 echo -e "\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL})${BOLD} Updating OS from repositories${RESET} (this may take a while depending on your Internet connection & Kali version/age)"
-wait 5s
+sleep 5
+export DEBIAN_FRONTEND=noninteractive
 apt -q update && APT_LISTCHANGES_FRONTEND=none apt -o Dpkg::Options::="--force-confnew" -y -q full-upgrade --fix-missing
-apt -y -q autoclean && apt -y -q autoremove
-
+apt -y -q autoremove && apt -y -q autoclean 
 
 ####Detect VM environment####
 #Only VMware supported for now
@@ -224,7 +224,7 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/gnome/endless-shapes.jpg'
 
 #Configure gnome favourites bar
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop', 'firefox-esr.desktop', 'org.gnome.Nautilus.desktop', 'kali-msfconsole.desktop', 'gnome-control-center.desktop', 'kali-burpsuite.desktop', 'sublime_text.desktop', 'atom.desktop']"
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop', 'firefox-esr.desktop', 'org.gnome.Nautilus.desktop', 'kali-msfconsole.desktop', 'gnome-control-center.desktop', 'kali-burpsuite.desktop', 'sublime_text.desktop', 'atom.desktop', 'SoapUI-5.5.0-0.desktop']"
 
 #Set theme (Kali Dark)
 gsettings set org.gnome.desktop.interface gtk-theme "Kali-X-Dark"
@@ -232,6 +232,8 @@ gsettings set org.gnome.desktop.interface gtk-theme "Kali-X-Dark"
 gsettings set org.gnome.desktop.interface icon-theme "Zen-Kali"
 #Set power saving time (15 minutes)
 gsettings set org.gnome.desktop.session idle-delay "uint32 900"
+#Move dock position to the right of the screen
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'RIGHT'
 #Enable date on clock panel
 dconf write /org/gnome/desktop/interface/clock-show-date "true"
 
@@ -251,7 +253,9 @@ wget -q https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/in
 chmod +x install.sh
 ./install.sh --unattended
 ZSH=${ZSH:-~/.oh-my-zsh}
-export SHELL="$ZSH"
+#export SHELL="$ZSH"
+# Change default shell to ZSH
+chsh -s /usr/bin/zsh
 #Fix .zsh path, add /root/.loca/bin to PATH
 sed -i '4iexport PATH=$PATH:/root/.local/bin' $HOME/.zshrc
 
@@ -293,13 +297,12 @@ echo -e "\n ${BLUE}[*]${RESET} (${STAGE}/${TOTAL}) Installing ${BLUE}crackmapexe
 apt install -y -q libssl-dev libffi-dev python-dev build-essential python-pip
 sleep 5
 pip install --user pipenv
-git clone --recursive https://github.com/byt3bl33d3r/CrackMapExec "$HOME/Downloads/"
+git clone --recursive https://github.com/byt3bl33d3r/CrackMapExec "$HOME/Downloads/CrackMapExec"
 
 file="$HOME/.local/bin/pipenv"
 if [ -s "$file" ]; then
 	cd "$HOME/Downloads/CrackMapExec" && "$file" install
-	"$file" shell
-	python setup.py install
+	"$file" run python setup.py install
 else
 	echo "${RED}[!]${RESET} Something went wrong. Installation of ${RED}${BOLD}CrackMapExec ${RESET}has failed!"
 fi
@@ -307,20 +310,33 @@ fi
 #### Install Winpayloads ####
 (( STAGE++ ))
 echo -e "\n ${BLUE}[*]${RESET} (${STAGE}/${TOTAL}) Installing ${BLUE}Winpayloads ${RESET}"
-#check if docker is running
-if [[ $(systemctl status docker) != *"active (running)"* ]]; then
-	echo "${YELLOW}[i]${RESET} starting docker service"
-	systemctl start docker
-	sleep 5
+# Check if docker is installed.
+if [[ $(dpkg -l | grep -i docker) != "" ]]; then
+  echo -e " ${GREEN}[+]${RESET} ${BOLD}Docker is already installed.${RESET}"
+  # Ckeck if service is running. If not start the service
+  if [[ $(systemctl status docker) != *"active (running)"* ]]; then
+  	echo "${YELLOW}[i]${RESET} starting docker service"
+  	systemctl start docker
+  	sleep 5
+  fi
+else
+  # Install docker from apt repositories
+  apt install -y -q docker.io
+  # Start the service. We know that is not running. No need to check
+  systemctl start docker
+  sleep 3s
 fi
 echo -e "${YELLOW}[i]${RESET} Pulling ${YELLOW}Docker image${RESET}..."
+# Pull Winpayloads docker image
 docker pull charliedean07/winpayloads:latest
+
 
 #### Init msfdb ####
 (( STAGE++ ))
 echo -e "\n ${BLUE}[*]${RESET} (${STAGE}/${TOTAL}) msfconsole setup ${RESET}"
 msfdb init
-if [ "$SHELL" = "/bin/zsh" ]; then echo 'alias msf="msfconsole"' >> $HOME/.zshrc; fi
+# Add alias for msfconsole in zshrc
+echo 'alias msf="msfconsole"' >> $HOME/.zshrc
 
 #Adding postgreSQL service to startup
 echo -e " ${YELLOW}[i]${RESET} Enabled ${YELLOW}postgresql ${RESET}service "
@@ -345,12 +361,12 @@ fi
 #Create directory structure to dowonload tools
 (( STAGE++ ))
 echo -e "\n ${BLUE}[*]${RESET} (${STAGE}/${TOTAL}) Creating tools directory"
-mkdir -p -v -Z /root/Tools/Webapp/ /root/Tools/Infrastructure/Linux /root/Tools/Infrastructure/Windows
+mkdir -p -v -Z /root/Tools/Webapp/CMS/Wordpress/ /root/Tools/Webapp/CMS/Drupal/ /root/Tools/OSINT/ /root/Tools/password-cracking/wordlists/ /root/Tools/Infrastructure/Linux /root/Tools/Infrastructure/Windows
 
 #Download tools
 pip install droopescan
 
-git clone https://github.com/immunIT/drupwn.git "$/root/Tools/Webapp/drupwn"
+git clone https://github.com/immunIT/drupwn.git "/root/Tools/Webapp/CMS/Drupal/drupwn"
 python3 setup.py install
 
 #ScoutSuite
@@ -373,7 +389,98 @@ rm "$HOME/Downloads/$file"
 
 (( STAGE++ ))
 echo -e "\n ${BLUE}[*]${RESET} (${STAGE}/${TOTAL}) ${BOLD}Installing useful Firefox addons${RESET}"
-#ToDO
+
+# Start Firefox for first time, background process
+firefox-esr &
+sleep 3s                    # Add delay to make sure that the FF profile has been created
+#--- Configure firefox
+echo -e "\n ${BLUE}[*]${RESET} Installing useful${BLUE}Firefox${RESET} addons"
+
+#--- Download extensions
+ffpath="$(find ~/.mozilla/firefox/*.default*/ -maxdepth 0 -mindepth 0 -type d -name '*.default*' -print -quit)/extensions"
+[ "${ffpath}" == "/extensions" ] && echo -e  "${RED}[!]${RESET} Couldn't find Firefox's config folder" 1>&2
+mkdir -p "${ffpath}/"
+
+# Wappalyzer
+echo -e " ${YELLOW}[i]${RESET} Downloading ${YELLOW}${BOLD}Wappalyzer${RESET}"
+timeout 300 curl --progress -k -L -f "https://addons.mozilla.org/firefox/downloads/file/3398489/wappalyzer-5.8.4-fx.xpi?src=dp-btn-primary" -o "$ffpath/wappalyzer@crunchlabz.com.xpi" || echo -e " ${RED}[!]${RESET} Issue downloading ${BOLD}Wappalyzer${RESET}" 1>&2
+# Foxyproxy standard
+echo -e " ${YELLOW}[i]${RESET} Downloading ${YELLOW}${BOLD}FoxyProxy standard${RESET}"
+timeout 300 curl --progress -k -L -f "https://addons.mozilla.org/firefox/downloads/file/3426955/foxyproxy_standard-7.4-an+fx.xpi?src=dp-btn-primary" -o "$ffpath/foxyproxy@eric.h.jung.xpi" || echo -e " ${RED}[!]${RESET} Issue downloading ${BOLD}FoxyProxy standard${RESET}" 1>&2
+# Cookies and headers analyser
+echo -e " ${YELLOW}[i]${RESET} Downloading ${YELLOW}${BOLD}Cookies and headers analyser${RESET}"
+timeout 300 curl --progress -k -L -f "https://addons.mozilla.org/firefox/downloads/file/858683/cookies_and_http_headers_analyser-2.6-an+fx-windows.xpi?src=dp-btn-primary" -o "$ffpath/{637ac5a9-47b3-475b-b724-f455f5a56897}.xpi" || echo -e " ${RED}[!]${RESET} Issue downloading ${BOLD}Cookies and HTTP headers analyser${RESET}" 1>&2
+# Web developer toolbar
+echo -e " ${YELLOW}[i]${RESET} Downloading ${YELLOW}${BOLD}Web developer toolbar${RESET}"
+timeout 300 curl --progress -k -L -f "https://addons.mozilla.org/firefox/downloads/file/773845/web_developer-2.0.1-an+fx.xpi?src=dp-btn-primary" -o "$ffpath/{c45c406e-ab73-11d8-be73-000a95be3b12}.xpi" || echo -e " ${RED}[!]${RESET} Issue downloading ${BOLD}Web developer toolbar${RESET}" 1>&2
+# Cookie editor
+echo -e " ${YELLOW}[i]${RESET} Downloading ${YELLOW}${BOLD}Cookie editor${RESET}"
+timeout 300 curl --progress -k -L -f "https://addons.mozilla.org/firefox/downloads/file/1132754/cookie_editor-0.1.3.1-an+fx.xpi?src=dp-btn-primary" -o "$ffpath/{48df221a-8316-4d17-9191-7fc5ea5f14c0}.xpi" || echo -e " ${RED}[!]${RESET} Issue downloading ${BOLD}Cookie editor${RESET}" 1>&2
+# React developer tools
+echo -e " ${YELLOW}[i]${RESET} Downloading ${YELLOW}${BOLD}React developer tools${RESET}"
+timeout 300 curl --progress -k -L -f "https://addons.mozilla.org/firefox/downloads/file/3417593/react_developer_tools-4.2.0-fx.xpi?src=dp-btn-primary" -o "$ffpath/@react-devtools.xpi" || echo -e " ${RED}[!]${RESET} Issue downloading ${BOLD}React developer tools${RESET}" 1>&2
+
+#--- Installing extensions
+for FILE in $(find "${ffpath}" -maxdepth 1 -type f -name '*.xpi'); do
+  d="$(basename "$FILE" .xpi)"
+  mkdir -p "${ffpath}/${d}/"
+  unzip -q -o -d "${ffpath}/${d}/" "$FILE"
+  rm -f "$FILE"
+done
+
+#Generate random key to use for foxyproxy configuration
+function random_key() {
+ od -A n -t u -N 4 /dev/urandom
+}
+
+
+# Kill firefox processes gracefully!
+kill -s SIGTERM $(ps -e | grep firefox-esr | awk '{print $1}')
+sleep 3s
+
+#Enable plugins
+FILE=$(find ~/.mozilla/firefox/*.default*/ -maxdepth 1 -type f -name 'extensions.json' -print -quit)
+if [ ! -e "$FILE" ] || [ -z "$FILE" ]; then
+  echo -e " ${RED}[!]${RESET}${BOLD} extensions.js${RESET} not found! "
+else
+  echo -e " ${GREEN}[+]${RESET} Enabled ${GREEN}Firefox's extensions${RESET} "
+  sed -i 's/"active":false,/"active":true,/g' "$FILE"                # Force them all!
+  sed -i 's/"userDisabled":true,/"userDisabled":false,/g' "$FILE"    # Force them all!
+fi
+
+# Enable addons. Firefox must be closed.
+FILE=$(find ~/.mozilla/firefox/*.default*/ -maxdepth 1 -type f -name 'prefs.js' -print -quit)
+if [ ! -e "$FILE" ] || [ -z "$FILE" ]; then
+  echo -e " ${RED}[!]${RESET}${BOLD} prefs.js${RESET} not found! "
+else
+  echo 'user_pref("extensions.autoDisableScopes", 14);' >> "$FILE"
+fi
+
+# Restarting Firfox
+echo -e " ${YELLOW}[i]${RESET} Restarting Firefox..."
+timeout 8 firefox >/dev/null 2>&1
+sleep 3s
+
+# Configure Foxyproxy
+FILE=$(find ~/.mozilla/firefox/*.default*/ -maxdepth 1 -type f -name 'storage-sync.sqlite')
+if [ ! -e "$FILE" ] || [ -z "$FILE" ]; then
+  echo -e " ${RED}[!]${RESET}Something went wrong with the${RED}${BOLD} FoxyProxy${RESET} Firefox extension (did any extensions install?).\n Skipping..." 1>&2
+else
+  # Flash the db. This will delete any existing entries
+  echo -e " ${YELLOW}[i]${RESET} Configuring FoxyProxy settings "
+
+  # backup old db and clean database
+  cp -n "$FILE"{,.buk} && sqlite3 "$FILE" "DELETE FROM collection_data WHERE record_id LIKE 'key-%'"
+
+  #Try to generate a random key value rather using hardcoded values
+  fp_key1=$(random_key | sed -e 's/^[[:space:]]*//')
+  fp_key2=$(random_key | sed -e 's/^[[:space:]]*//')
+  fp_key3=$(random_key | sed -e 's/^[[:space:]]*//')
+  fp_key4=$(random_key | sed -e 's/^[[:space:]]*//')
+
+  #Insert proxy settings to db
+  sqlite3 "$FILE" "INSERT into collection_data(collection_name, record_id, record) VALUES ('default/foxyproxy@eric.h.jung','key-$fp_key1', '{\"id\":\"$fp_key1\",\"key\":\"$fp_key1\",\"data\":{\"title\":\"localhost 8080\",\"type\":1,\"color\":\"#1a14cc\",\"address\":\"127.0.0.1\",\"port\":8080,\"active\":true,\"whitePatterns\":[{\"title\":\"all URLs\",\"active\":true,\"pattern\":\"*\",\"type\":1,\"protocols\":1}],\"blackPatterns\":[{\"title\":\"local hostnames (usually no dots in the name). Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?(?:localhost|127\\\\.\\\\d+\\\\.\\\\d+\\\\.\\\\d+)(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1},{\"title\":\"local subnets (IANA reserved address space). Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?(?:192\\\\.168\\\\.\\\\d+\\\\.\\\\d+|10\\\\.\\\\d+\\\\.\\\\d+\\\\.\\\\d+|172\\\\.(?:1[6789]|2[0-9]|3[01])\\\\.\\\\d+\\\\.\\\\d+)(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1},{\"title\":\"localhost - matches the local host optionally prefixed by a user:password authentication string and optionally suffixed by a port number. The entire local subnet (127.0.0.0/8) matches. Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?[\\\\w-]+(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1}],\"index\":0},\"_status\":\"created\"}'); INSERT into collection_data(collection_name, record_id, record) VALUES ('default/foxyproxy@eric.h.jung','key-$fp_key2', '{\"id\":\"key-$fp_key2\",\"key\":\"$fp_key2\",\"data\":{\"title\":\"localhost 8081\",\"type\":1,\"color\":\"#66cc66\",\"address\":\"127.0.0.1\",\"port\":8081,\"active\":true,\"whitePatterns\":[{\"title\":\"all URLs\",\"active\":true,\"pattern\":\"*\",\"type\":1,\"protocols\":1}],\"blackPatterns\":[{\"title\":\"local hostnames (usually no dots in the name). Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?(?:localhost|127\\\\.\\\\d+\\\\.\\\\d+\\\\.\\\\d+)(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1},{\"title\":\"local subnets (IANA reserved address space). Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?(?:192\\\\.168\\\\.\\\\d+\\\\.\\\\d+|10\\\\.\\\\d+\\\\.\\\\d+\\\\.\\\\d+|172\\\\.(?:1[6789]|2[0-9]|3[01])\\\\.\\\\d+\\\\.\\\\d+)(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1},{\"title\":\"localhost - matches the local host optionally prefixed by a user:password authentication string and optionally suffixed by a port number. The entire local subnet (127.0.0.0/8) matches. Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?[\\\\w-]+(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1}],\"index\":1},\"_status\":\"created\"}'); INSERT into collection_data(collection_name, record_id, record) VALUES ('default/foxyproxy@eric.h.jung','key-$fp_key3', '{\"id\":\"key-$fp_key3\",\"key\":\"$fp_key3\",\"data\":{\"title\":\"localhost 8069\",\"type\":1,\"color\":\"#ccac2a\",\"address\":\"127.0.0.1\",\"port\":8069,\"active\":true,\"whitePatterns\":[{\"title\":\"all URLs\",\"active\":true,\"pattern\":\"*\",\"type\":1,\"protocols\":1}],\"blackPatterns\":[{\"title\":\"local hostnames (usually no dots in the name). Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?(?:localhost|127\\\\.\\\\d+\\\\.\\\\d+\\\\.\\\\d+)(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1},{\"title\":\"local subnets (IANA reserved address space). Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?(?:192\\\\.168\\\\.\\\\d+\\\\.\\\\d+|10\\\\.\\\\d+\\\\.\\\\d+\\\\.\\\\d+|172\\\\.(?:1[6789]|2[0-9]|3[01])\\\\.\\\\d+\\\\.\\\\d+)(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1},{\"title\":\"localhost - matches the local host optionally prefixed by a user:password authentication string and optionally suffixed by a port number. The entire local subnet (127.0.0.0/8) matches. Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?[\\\\w-]+(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1}],\"index\":2},\"_status\":\"created\"}'); INSERT into collection_data(collection_name, record_id, record) VALUES ('default/foxyproxy@eric.h.jung','key-$fp_key4', '{\"id\":\"$fp_key4\",\"key\":\"$fp_key4\",\"data\":{\"title\":\"localhost 6969\",\"type\":1,\"color\":\"#c730cc\",\"address\":\"127.0.0.1\",\"port\":6969,\"active\":true,\"whitePatterns\":[{\"title\":\"all URLs\",\"active\":true,\"pattern\":\"*\",\"type\":1,\"protocols\":1}],\"blackPatterns\":[{\"title\":\"local hostnames (usually no dots in the name). Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?(?:localhost|127\\\\.\\\\d+\\\\.\\\\d+\\\\.\\\\d+)(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1},{\"title\":\"local subnets (IANA reserved address space). Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?(?:192\\\\.168\\\\.\\\\d+\\\\.\\\\d+|10\\\\.\\\\d+\\\\.\\\\d+\\\\.\\\\d+|172\\\\.(?:1[6789]|2[0-9]|3[01])\\\\.\\\\d+\\\\.\\\\d+)(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1},{\"title\":\"localhost - matches the local host optionally prefixed by a user:password authentication string and optionally suffixed by a port number. The entire local subnet (127.0.0.0/8) matches. Pattern exists because ''Do not use this proxy for localhost and intranet/private IP addresses'' is checked.\",\"active\":true,\"pattern\":\"^(?:[^:@/]+(?::[^@/]+)?@)?[\\\\w-]+(?::\\\\d+)?(?:/.*)?$\",\"type\":2,\"protocols\":1}],\"index\":3},\"_status\":\"created\"}');" ".exit"
+fi
 
 
 #### Install Nessus ####
@@ -381,24 +488,38 @@ echo -e "\n ${BLUE}[*]${RESET} (${STAGE}/${TOTAL}) ${BOLD}Installing useful Fire
 echo -e "\n ${BLUE}[*]${RESET} (${STAGE}/${TOTAL}) ${BOLD}Installing ${BLUE}Nessus${RESET}"
 
 #Hardcoded version number
-wget -c "https://www.tenable.com/downloads/api/v1/public/pages/nessus/downloads/9745/download?i_agree_to_tenable_license_agreement=true" -O $HOME/Downloads/Nessus-8.5.1-debian6_amd64.deb
+wget -c "https://www.tenable.com/downloads/api/v1/public/pages/nessus/downloads/10079/download?i_agree_to_tenable_license_agreement=true" -O $HOME/Downloads/Nessus-8.5.1-debian6_amd64.deb
 dpkg -i $HOME/Downloads/Nessus-*-debian6_amd64.deb
+sleep 3
+
+if [ -f "/opt/nessus/sbin/nessuscli" ]; then
+  if [ ! -z "$nessusKey" ]; then
+  	/opt/nessus/sbin/nessuscli fetch --register $nessusKey
+  	/opt/nessus/sbin/nessusd -R
+  	/opt/nessus/sbin/nessus-service -D
+  	xdg-open https://127.0.0.1:8834/
+  else
+    echo -e " ${YELLOW}[*]${RESET} ${BOLD}Nessus has been installed but has not been activated${RESET}"
+    echo -e " ${RED}[!]${RESET} ${BOLD}A Nessus license was not provided!${RESET}"
+  fi
+else
+  echo -e " ${RED}[*]${RESET} ${BOLD}Oops! Something went wrong. ${RED}${BOLD}Nessus has not been installed${RESET}"
+fi
 
 #Cleaning up
 rm $HOME/Downloads/Nessus-*-debian6_amd64.deb
 #Starting the service
 systemctl start nessusd
-wait 3
-
+sleep 3
 
 if [ ! -z "$nessusKey" ]; then
 	/opt/nessus/sbin/nessuscli fetch --register $nessusKey
 	/opt/nessus/sbin/nessusd -R
 	/opt/nessus/sbin/nessus-service -D
 	xdg-open https://127.0.0.1:8834/  #leave service running
-else
-  echo -e " ${RED}[!]${RESET} ${BOLD}Nessus license not provided!${RESET}"
-  echo -e " ${YELLOW}[*]${RESET} ${BOLD}Nessus has been installed but has not been activated${RESET}"
+elif
+
+
 fi
 
 #Download latest Nessus pro for debian/kali
@@ -453,7 +574,7 @@ echo -e "\n ${BLUE}[*]${RESET} (${STAGE}/${TOTAL}) ${BOLD}Generating SSH keys"
 # Wipe existing openssh keys
 rm -f /ect/ssh/ssh_host_*
 # Backup old user keys
-mkdir /root/.ssh/old_keys
+mkdir -p /root/.ssh/old_keys/
 for file in $(find /root/.ssh/ -type f ! -name authorized_keys)
 do
   mv $file /root/.ssh/old_keys/`basename $file`.old
